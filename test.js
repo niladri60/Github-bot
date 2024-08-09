9235a690cfd4b258e3093d3df58d1630c2197fc5
Niladri Nandy <niladri.nandy2names@gmail.com>
2024-08-09 22:36:51 +0530

    Update test.js
    a new function added.
    
    Co-Authored-By: Niladri Nandy <100217492+niladri60@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,2 +1,6 @@
-console.log("This is Test file2")
-console.log("This is Test file3")
+console.log("This is Test file2");
+console.log("This is Test file3");
+
+function test() {
+  console.log("This function is to get the batch");
+}

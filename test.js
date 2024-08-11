123093e6e7432ecf1e48a48e4eff69acb8010165
Niladri Nandy <100217492+niladri60@users.noreply.github.com>
2024-08-11 10:38:51 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ console.log("This is Test file2");
 console.log("This is Test file3");
 
 function test() {
-  console.log("This function is to get the batch1");
+  console.log("This function is to get the batch2");
 }

23c652b938fe4e1459e5b798c7f0be006d0cea0d
Niladri Nandy <100217492+niladri60@users.noreply.github.com>
2024-08-13 23:29:26 +0530

    Add comment to test.js file

--- test.js
+++ test.js
@@ -4,3 +4,5 @@ console.log("This is Test file3");
 function test() {
   console.log("This function is to get the batch2");
 }
+
+//This is the default js file we need to create everyday to check for the streak.

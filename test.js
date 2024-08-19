363fbb263c189d2125c095f006fd43df2eef784b
Niladri Nandy <100217492+niladri60@users.noreply.github.com>
2024-08-19 22:39:48 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,4 +5,4 @@ function test() {
   console.log("This function is to get the batch2");
 }
 
-//This is the default js file we need to create everyday to check for the streak3.
+//This is the default js file we need to create everyday to check for the streak4.
